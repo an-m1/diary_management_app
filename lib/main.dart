3fb389bbfc63375b0b718278b006e2b5dad27473
Ankit Modhera <ankitmodhera@Ankits-MacBook-Pro.local>
2025-01-22 23:10:28 -0500

    basic diary/notetaking app implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,437 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => DiaryState(),
+      child: MaterialApp(
+        title: 'Note Taking App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        ),
+        home: MyHomePage(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+class Note {
+  String title;
+  String body;
+  DateTime created;
+  DateTime modified;
+  bool isFavorite;
+
+  Note({
+    required this.title,
+    required this.body,
+    required this.created,
+    required this.modified,
+    this.isFavorite = false,
+  });
+}
+
+class DiaryState extends ChangeNotifier {
+  List<Note> notes = [];
+
+  void addNote(String title, String body) {
+    notes.insert(0, Note(
+      title: title,
+      body: body,
+      created: DateTime.now(),
+      modified: DateTime.now(),
+    ));
+    notifyListeners();
+  }
+
+  void updateNote(Note note, String newTitle, String newBody) {
+    note.title = newTitle;
+    note.body = newBody;
+    note.modified = DateTime.now();
+    notifyListeners();
+  }
+
+  void deleteNote(Note note) {
+    notes.remove(note);
+    notifyListeners();
+  }
+
+  void toggleFavorite(Note note) {
+    note.isFavorite = !note.isFavorite;
+    notifyListeners();
+  }
+
+  List<Note> get sortedNotes {
+    return notes
+      ..sort((a, b) {
+        if (a.isFavorite != b.isFavorite) {
+          return b.isFavorite ? 1 : -1;
+        }
+        return b.modified.compareTo(a.modified);
+      });
+  }
+}
+
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  var selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var colorScheme = Theme.of(context).colorScheme;
+
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = NotesPage();
+        break;
+      case 1:
+        page = AllNotesPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    var mainArea = ColoredBox(
+      color: colorScheme.surfaceVariant,
+      child: AnimatedSwitcher(
+        duration: Duration(milliseconds: 200),
+        child: page,
+      ),
+    );
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 450) {
+            return Column(
+              children: [
+                Expanded(child: mainArea),
+                SafeArea(
+                  child: BottomNavigationBar(
+                    items: [
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.home),
+                        label: 'Notes',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.list),
+                        label: 'All Notes',
+                      ),
+                    ],
+                    currentIndex: selectedIndex,
+                    onTap: (value) {
+                      setState(() {
+                        selectedIndex = value;
+                      });
+                    },
+                  ),
+                )
+              ],
+            );
+          } else {
+            return Row(
+              children: [
+                SafeArea(
+                  child: NavigationRail(
+                    extended: constraints.maxWidth >= 600,
+                    destinations: [
+                      NavigationRailDestination(
+                        icon: Icon(Icons.home),
+                        label: Text('Notes'),
+                      ),
+                      NavigationRailDestination(
+                        icon: Icon(Icons.list),
+                        label: Text('All Notes'),
+                      ),
+                    ],
+                    selectedIndex: selectedIndex,
+                    onDestinationSelected: (value) {
+                      setState(() {
+                        selectedIndex = value;
+                      });
+                    },
+                  ),
+                ),
+                Expanded(child: mainArea),
+              ],
+            );
+          }
+        },
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+}
+
+class NotesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var diaryState = context.watch<DiaryState>();
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 3,
+            child: ListView.builder(
+              reverse: true,
+              itemCount: diaryState.notes.length,
+              itemBuilder: (context, index) {
+                var note = diaryState.notes[index];
+                return ListTile(
+                  title: Text(note.title),
+                  subtitle: Text(DateFormat('yyyy-MM-dd HH:mm').format(note.modified)),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: Icon(note.isFavorite ? Icons.favorite : Icons.favorite_border),
+                        onPressed: () {
+                          diaryState.toggleFavorite(note);
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              final TextEditingController _titleController = TextEditingController(text: note.title);
+                              final TextEditingController _bodyController = TextEditingController(text: note.body);
+
+                              return AlertDialog(
+                                title: Text('Edit Note'),
+                                content: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    TextField(
+                                      controller: _titleController,
+                                      decoration: InputDecoration(
+                                        border: OutlineInputBorder(),
+                                        labelText: 'Title',
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    TextField(
+                                      controller: _bodyController,
+                                      decoration: InputDecoration(
+                                        border: OutlineInputBorder(),
+                                        labelText: 'Body',
+                                      ),
+                                      maxLines: 5,
+                                    ),
+                                  ],
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text('Cancel'),
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      if (_titleController.text.isNotEmpty && _bodyController.text.isNotEmpty) {
+                                        diaryState.updateNote(note, _titleController.text, _bodyController.text);
+                                        Navigator.of(context).pop();
+                                      }
+                                    },
+                                    child: Text('Save'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          diaryState.deleteNote(note);
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+          SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  final TextEditingController _titleController = TextEditingController();
+                  final TextEditingController _bodyController = TextEditingController();
+
+                  return AlertDialog(
+                    title: Text('New Note'),
+                    content: SingleChildScrollView(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextField(
+                            controller: _titleController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Title',
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          TextField(
+                            controller: _bodyController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Body',
+                            ),
+                            maxLines: 5,
+                          ),
+                        ],
+                      ),
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Cancel'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_titleController.text.isNotEmpty && _bodyController.text.isNotEmpty) {
+                            diaryState.addNote(
+                              _titleController.text,
+                              _bodyController.text,
+                            );
+                            Navigator.of(context).pop();
+                          }
+                        },
+                        child: Text('Add Note'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            child: Text('Add Note'),
+          ),
+          Spacer(flex: 2),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class AllNotesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var diaryState = context.watch<DiaryState>();
+
+    return ListView(
+      padding: const EdgeInsets.all(16.0),
+      children: diaryState.sortedNotes.map((note) {
+        return ListTile(
+          title: Text(note.title),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Created: ${DateFormat('yyyy-MM-dd HH:mm').format(note.created)}'),
+              Text('Modified: ${DateFormat('yyyy-MM-dd HH:mm').format(note.modified)}'),
+            ],
+          ),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                icon: Icon(note.isFavorite ? Icons.favorite : Icons.favorite_border),
+                onPressed: () {
+                  diaryState.toggleFavorite(note);
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.edit),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      final TextEditingController _titleController = TextEditingController(text: note.title);
+                      final TextEditingController _bodyController = TextEditingController(text: note.body);
+
+                      return AlertDialog(
+                        title: Text('Edit Note'),
+                        content: SingleChildScrollView(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextField(
+                                controller: _titleController,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Title',
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              TextField(
+                                controller: _bodyController,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Body',
+                                ),
+                                maxLines: 5,
+                              ),
+                            ],
+                          ),
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('Cancel'),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              if (_titleController.text.isNotEmpty && _bodyController.text.isNotEmpty) {
+                                diaryState.updateNote(note, _titleController.text, _bodyController.text);
+                                Navigator.of(context).pop();
+                              }
+                            },
+                            child: Text('Save'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () {
+                  diaryState.deleteNote(note);
+                },
+              ),
+            ],
+          ),
+        );
+      }).toList(),
     );
   }
 }
